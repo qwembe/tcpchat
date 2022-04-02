f46356f86f7010f017ec1ab88cb78e0763cf4e17
Андрей Вайгачёв <qwembe@gmail.com>
2022-04-02 19:26:25 +0300

    init

---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 5555
+
+
+class Server:
+    sock = socket.socket()
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
